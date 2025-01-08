c317c136a8fc69520a56ebbdd14eddf11bb9577d
raja-delhi <nonofficial1234098@gmail.com>
2025-01-08 21:40:49 +0530

    Creating and adding all microservices for ecommerce.

---
+++ payment-history-service/src/main/java/com/ecommerce/payment_history_service/PaymentHistoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.payment_history_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentHistoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentHistoryServiceApplication.class, args);
+	}
+
+}
