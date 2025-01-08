f30dd24b83efb88c7d141c624a017d358829b483
raja-delhi <nonofficial1234098@gmail.com>
2025-01-08 22:31:33 +0530

    Base Code checkin of User Auth microservice

---
+++ user-auth-service/src/main/java/com/ecommerce/user/auth/controller/AuthController.java
@@ -0,0 +1,21 @@
+package com.ecommerce.user.auth.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username, @RequestParam String password) {
+        return "Login successful for " + username;
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestParam String username, @RequestParam String password) {
+        return "User registered successfully";
+    }
+}
